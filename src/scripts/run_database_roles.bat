d6fbac50fa7f8bc43a821bc456a7778814bf9d98
Robbie Luijben <diggingforfire@gmail.com>
2021-10-22 23:19:04 +0200

    scrippies

---
+++ src/scripts/run_database_roles.bat
@@ -0,0 +1,2 @@
+:: executes the compiled and packaged printserver application
+java -classpath PrintServer.jar;Action.jar;LoginModule.jar;sqlite-jdbc-3.36.0.jar -Djava.security.manager -Djava.security.policy==printserver_role_based.policy -Djava.security.auth.login.config==printserver_jaas_database_roles.config printserver.Main usedatabase
---
+++ src/scripts/run_hardcoded.bat
@@ -0,0 +1,2 @@
+:: executes the compiled and packaged printserver application
+java -classpath PrintServer.jar;Action.jar;LoginModule.jar;sqlite-jdbc-3.36.0.jar -Djava.security.manager -Djava.security.policy==printserver_user_based.policy -Djava.security.auth.login.config==printserver_jaas_hardcoded.config printserver.Main
